d82710e2b88e0d1f7640235cda0cfa95b5da7b5e
SuperAIdesu <tjx10011@gmail.com>
2023-11-01 15:23:57 -0400

    update scraping script for dalle2

--- scrape.py
+++ scrape.py
@@ -18,7 +18,7 @@ def call_api(param):
         cur_flair = submission["link_flair_text"]
         cur_utc = submission["created_utc"]
         media_ls = submission["media_metadata"] if "media_metadata" in submission.keys() else None
-        if cur_flair != param["flair"]:
+        if param["flair"] is not None and cur_flair != param["flair"]:
             continue
         if media_ls is None:
             continue
@@ -60,10 +60,10 @@ def call_api(param):
     return df, int(cur_utc)
 
 
-def scraping_loop(subreddit, flair, max_num=30000):
+def scraping_loop(subreddit, flair, max_num=30000, output_name=None, before=None):
     collected_all = []
     collected_len = 0
-    last_timestamp = int(time.time())
+    last_timestamp = int(time.time()) if before is None else before
     param = {
         "subreddit": subreddit,
         "flair": flair,
@@ -71,6 +71,9 @@ def scraping_loop(subreddit, flair, max_num=30000):
     }
     while collected_len < max_num:
         collected_df, last_timestamp = call_api(param)
+        if collected_df.shape[0] == 0:
+            print("No more data, saving current data and exiting...")
+            break
         collected_all.append(collected_df)
         collected_len += collected_df.shape[0]
         print(f"collected_len: {collected_len}, last_timestamp: {last_timestamp}")
@@ -88,19 +91,20 @@ def scraping_loop(subreddit, flair, max_num=30000):
             separator=""
         ).alias("url")
     ).select("time", "url", "post_id", "post_title")
-    df.write_parquet(f"urls/{subreddit}.parquet")
-    df.select("url").write_csv(f"urls/{subreddit}.csv", has_header=False)
+    if output_name is None:
+        output_name = subreddit
+    df.write_parquet(f"urls/{output_name}.parquet")
+    df.select("url").write_csv(f"urls/{output_name}.csv", has_header=False)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--subreddit", help="subreddit name")
-    parser.add_argument("--flair", help="flair filter")
-    parser.add_argument("--max_num", help="max number of posts to scrape", default=30000)
+    parser.add_argument("--flair", help="flair filter", default=None, type=str)
+    parser.add_argument("--max_num", help="max number of posts to scrape", default=30000, type=int)
+    parser.add_argument("--output_name", help="custom output name", default=None)
+    parser.add_argument("--before", help="before timestamp", default=None, type=int)
 
     args = parser.parse_args()
-    subreddit = args.subreddit
-    flair = args.flair
-    max_num = int(args.max_num)
 
-    scraping_loop(subreddit, flair, max_num)
+    scraping_loop(**args.__dict__)
